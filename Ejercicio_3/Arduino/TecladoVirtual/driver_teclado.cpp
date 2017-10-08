5d04ed330974621ea4965e86448bbc5419470310
lelalexi <32463563+lelalexi@users.noreply.github.com>
2017-10-08 18:39:11 -0300

    Apartado 2
    Libreria teclado.
    Implementada para el Shield LCD1602

---
+++ Ejercicio_3/Arduino/TecladoVirtual/driver_teclado.cpp
@@ -0,0 +1,99 @@
+#include "driver_teclado.h"
+#include "driver_adc.h"
+#include "Arduino.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int adc_key_val[5] ={30, 170, 360, 535, 760 };
+int NUM_KEYS = 5;
+int key = -1, oldKey = -1;
+void (*keyup_handler[6])(void);
+void (*keydown_handler[6])(void);
+int count = 0;
+volatile int boton = 0;
+int *virtualKey;
+adc_cfg cfg;
+
+void key_down_callback(void (*handler)(), int tecla)
+{
+  keydown_handler[tecla] = handler;
+}
+
+void key_up_callback(void (*handler)(), int tecla)
+{
+  keyup_handler[tecla] = handler;
+}
+
+int get_key(int input)
+{
+  int k;
+  for (k = 0; k < NUM_KEYS; k++)
+    if (input < adc_key_val[k])
+      return k;
+
+  if (k >= NUM_KEYS)
+    k = -1;     // No valid key pressed
+  
+  return k;
+}
+
+void Callback()
+{
+  key = get_key(cfg.aVal);
+  count = (count + 1) % 2;
+  delay(50);
+  if(count)
+    return;
+  if(key!=oldKey)
+  {
+    if(key >= 0)
+      keydown_handler[key]();
+    else
+      keyup_handler[oldKey]();
+    
+    oldKey = key;
+  }
+  //PARA SIMULAR EL APRETADO Y SOLTADO
+  if(*virtualKey >= 0)
+  {
+    keydown_handler[*virtualKey]();
+    delay(1000);
+    keyup_handler[*virtualKey](); 
+    *virtualKey = -1;
+  }
+}
+
+void teclado_init(int *vKey)
+{
+  virtualKey = vKey;
+  DDRC &= ~(1 << DDC2);      // PC2 (PCINT10 pin) is now an input
+  PCICR |= (1 << PCIE1);     // set PCIE1 to enable PCMSK1 scan
+  PCMSK1 |= (1 << PCINT10);   // set PCINT10 to trigger an interrupt on state change
+
+  cfg.channel = 0;
+  cfg.callback = Callback;
+  adc_init(&cfg);
+  
+}
+
+void teclado_loop()
+{
+  adc_loop();
+  if(boton)
+  {
+    if(PINC & (1 << DDC2))
+      keydown_handler[BOTON_A2]();
+    else
+      keyup_handler[BOTON_A2]();
+    boton = 0;
+  }
+}
+
+
+ISR (PCINT1_vect)
+{
+  boton = 1;
+}
+
+
+
---
+++ Ejercicio_3/Arduino/TecladoVirtual/driver_teclado.h
@@ -0,0 +1,19 @@
+#ifndef DRIVER_TECLADO
+#define DRIVER_TECLADO
+
+#define TECLA_RIGHT 0
+#define TECLA_UP 1
+#define TECLA_DOWN 2
+#define TECLA_LEFT 3
+#define TECLA_SELECT 4
+#define BOTON_A2 5
+
+void teclado_init(int *vKey);
+
+void teclado_loop();
+
+void key_down_callback(void (*handler)(), int tecla);
+
+void key_up_callback(void (*handler)(), int tecla);
+
+#endif
